74160f8c0e25520671b6ce42b5fb80f9e58c8f31
van-ter <ivantr1995@gmail.com>
2019-04-05 10:41:43 +0300

    last version

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "./"
+    });
+
+    gulp.watch("./scss/*.scss", ['sass']);
+    gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("./scss/*.scss")
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest("./css"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve']);
---
+++ js/script.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+  $('.sandwich').click(function(){
+    $('nav').toggleClass('adaptive');
+  });
+
+  $('.i-search').click(function(){
+    if($(this).hasClass('search-false')){
+      $(this).removeClass('search-false')
+      $(this).parent('.wrapper-search').addClass('show-search')
+    }
+  });
+
+  $(document).mouseup(function (e){ 
+    var div = $(".i-search, input"); 
+    if (!div.is(e.target) && div.has(e.target).length === 0) { 
+      $(".i-search").addClass('search-false')
+      $(".i-search").parent('.wrapper-search').removeClass('show-search')
+    }
+    // var div2 = $("nav"); 
+    // if (!div2.is(e.target) && (div2.hasClass('adaptive')) && div2.has(e.target).length === 0) { 
+    //   $('nav').toggleClass('adaptive');
+    //   $('.sandwich').toggleClass('close');
+    // }
+  });
+  var $headerH = $('header').height();
+  $('header').css('height', $headerH);
+
+  $(window).on('scroll load', function () {
+    if ($(document).scrollTop() > 5) {
+      $(".main-header").addClass("fixed");
+      $('.home-top').addClass('scrol');
+      $('.baner .btn').addClass('btn-red');
+    } else {
+      $(".main-header").removeClass("fixed");
+      $('.home-top').removeClass('scrol');
+      $('.baner .btn').removeClass('btn-red')
+    }
+  });
+
+  $('#form').magnificPopup({
+    type: 'inline',
+    preloader: false
+  });
+
+  $('#designe').load('../form-blade.html');
+
+  $('.sandwich').click(function(){
+    $(this).toggleClass('close');
+    $(this).closest('nav').toggleClass('adaptive');
+  });
+});
